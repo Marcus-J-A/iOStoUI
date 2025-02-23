6b2247e9db82e0fe2829857a982ee1bc2dc1370b
Luke <zkluuke@gmail.com>
2025-02-23 18:00:24 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+![iOS2UI icon](https://raw.githubusercontent.com/Marcus-J-A/iOStoUI/refs/heads/main/icon-2.png)
+# iOS2UI
+> Dualboot utility for booting NonUI & iOS.
+
+## Requirements
+
+**On Device**
+
+> 1. OpenSSH - allows you access device's command shell remotely and transfer files. Available in Cydia
+> 2. dualbootstuff package. It contains gptfdisk, hfs_resize, kloader and other tools we'll use. Available in this Cydia repository (nyansatan.github.io/apt)
+> 3. diskdev-cmds package. Just for umount. Available in Cydia
+> 4. nano - CLI text editor we'll use it to edit fstab and launchd.conf. Available in Cydia
+
+**Device Requirements**
+> iPhone 4 - 5
+
+## How to use
+> Just Run iOS2Ui.sh
+
+## Credits
+
+**@danzatt - for original partitioning guide and hfs_resize**
+
+**Roderick W. Smith - for gptfdisk**
+
+**@winocm, @xerub - for kloader**
+
+**@ShadowLee19 - for NOR-flashing untether method**
+
+**@axi0mX - for iOS 4/5 compatible kloader and firmware keys for S5L8942 devices**
+
+**@NyanSatan - iOS Dualboot Guide**
+
+**@stonneph - Figuring out on booting NonUI**
+
+**@LukeZGD - Fixing the script**
---
+++ iOS2UI.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/expect -f
+
+# Ask for the IP address
+puts -nonewline "Enter the IP address of the remote device: "
+flush stdout
+gets stdin DEVICE_IP
+
+# Set SSH password (default is alpine)
+set PASSWORD "alpine"
+
+# Spawn SSH session
+spawn ssh root@$DEVICE_IP
+
+# Handle password prompt
+expect {
+    "*password:*" {
+        send "$PASSWORD\r"
+        expect "# "  ; # Wait for shell prompt
+    }
+}
+
+# Resize HFS partition
+send "hfs_resize /private/var 1505306368\r"
+expect "# "  
+
+# Run gptfdisk interactively
+send "gptfdisk /dev/rdisk0s1\r"
+expect "Command (? for help): "  
+
+# Send gptfdisk commands
+send "i\r"
+set timeout -1
+expect "Partition number (1-4): "  
+send "2\r"
+expect "Command (? for help): "  
+send_user "\nCopy the Partition GUID Code at the command above, we will ask you for it later on (Press Enter to continue)"
+expect_user "\n"
+send "d\r"
+expect "Partition number (1-4): "  
+send "2\r"
+expect "Command (? for help): "  
+send "n\r"
+expect "Partition number (2-4, default 2):"
+send "\r"
+expect "First sector"
+send "\r"
+expect "Last sector"
+send "1364612\r"
+expect "Hex code or GUID (L to show codes, Enter = AF00)"
+send "AF00\r"
+expect "Command (? for help): "
+send "c\r"
+expect "Partition number (1-4): "
+send "2\r"
+expect "Enter name: "
+send "Data\r"
+expect "Command (? for help): "
+send "x\r"
+send "2\r"
+send "48\r"
+send "49\r"
+send "\r"
+send "\r"
+puts -nonewline "Enter the Partition unique GUID you saved earlier: "
+flush stdout
+gets stdin GUID_UNIQUE
+send "$GUID_UNIQUE\r"
+expect eof
